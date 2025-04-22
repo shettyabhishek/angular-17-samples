4ae0758c5df64372eb09f718e5d824c6918cc44c
Abhishek <shettyabhishek829@yahoo.in>
2025-04-22 20:10:52 +0530

    Implemented ligh / dark mode

--- src/app/components/tailwind/tlwcomps/smpl-web-pgs/sample-three/sample-three.component.ts
+++ src/app/components/tailwind/tlwcomps/smpl-web-pgs/sample-three/sample-three.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sample-three',
@@ -6,6 +6,27 @@ import { Component } from '@angular/core';
   templateUrl: './sample-three.component.html',
   styleUrl: './sample-three.component.scss'
 })
-export class SampleThreeComponent {
+export class SampleThreeComponent implements OnInit{
+  private _currentMode = localStorage.getItem('color-theme');
 
+  ngOnInit(): void {
+    if(this._currentMode && this._currentMode === 'dark'){
+      document.documentElement.setAttribute('data-theme','dark');
+      localStorage.setItem('color-theme','dark');
+    }
+  }
+
+  //Method to switch the display mode
+  switchMode(){
+    if(this._currentMode === 'dark'){
+      document.documentElement.removeAttribute('data-theme');
+      localStorage.removeItem('color-theme');
+      this._currentMode = '';
+    }
+    else{
+      document.documentElement.setAttribute('data-theme','dark');
+      localStorage.setItem('color-theme','dark');
+      this._currentMode = 'dark';
+    }
+  }
 }
