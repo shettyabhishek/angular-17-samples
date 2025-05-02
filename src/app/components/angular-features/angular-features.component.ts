24f9b59d1cec8d2d94d0f81ed3e42f5d55d31fcc
Abhishek <shettyabhishek829@yahoo.in>
2025-05-02 22:29:17 +0530

    Added new angular samples

--- src/app/components/angular-features/angular-features.component.ts
+++ src/app/components/angular-features/angular-features.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, signal, WritableSignal } from '@angular/core';
 import { AlphaNumDirective } from './directives/alpha-num.directive';
 
 @Component({
@@ -16,6 +16,25 @@ export class AngularFeaturesComponent {
   switchCondition: string = '';
   showCodePreview: boolean = false;
   imgCodePreview: string = '';
+
+  //implemented through signal
+  counterSignal: WritableSignal<number> = signal<number>(0);
+  translations: WritableSignal<{name: string,age: number}> = signal<{name: string,age: number}>({name: 'Test user',age: 10});
+  //below method updates a signal which holds an numeric (primitive data)
+  increment():WritableSignal<number>{
+    this.counterSignal.set(this.counterSignal()+1);
+    return this.counterSignal;
+  }
+  //below method updates a signal which holds an object value
+  changeUser():WritableSignal<{}>{
+    this.translations.update((item)=>({...item,name:'John Doe',age: 25}));
+    return this.translations;
+  }
+  derivedCounterSignal = computed(()=>{
+    console.log("The compute method is triggered");
+    return this.counterSignal;
+  })
+
 
   showHidePopUp(type: string):void {
     this.imgCodePreview = type;
